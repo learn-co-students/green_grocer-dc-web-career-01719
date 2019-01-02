5dae29240233a1aec9e99ae58caa0e74071d335c
jameseclement <jameseclement@gmail.com>
2019-01-02 02:14:23 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,47 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  
+## creates cart to be output at the end
+  consolidated_cart = {}
+  
+## iterates over each item and all its data
+  cart.each do |item|
+    
+## for each item in the cart, do something to just its name, then the price info
+    item.each do |name, price|
+      binding.pry
+      
+## if the name of the item is not already a key in consolidated_cart
+      if consolidated_cart[name].nil?
+        ## make the name a key, and make its values the price info plus the count of 1 
+        consolidated_cart[name] = price.merge({:count => 1})
+      else
+        consolidated_cart[name][:count] += 1
+      end
+    end
+  end
+  consolidated_cart
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_coupons(cart, coupon)
+  coupon.each do |item|
+    name_of_item = item[:item]
+    if cart.has_key?(name_of_item) == true && cart[name_of_item][:count] >= item[:num]
+      cart[name_of_item][:count] = cart[name_of_item][:count] - item[:num]
+      new_item = name_of_item + (" W/COUPON")
+      puts cart.has_key?(new_item)
+      if cart.has_key?(new_item) == false
+        cart[new_item] = {:price => item[:cost], :clearance => cart[name_of_item][:clearance], :count => 1}
+      else 
+        cart[new_item][:count] += 1
+      end
+    end
+  end
+  cart
 end
+  
+
 
 def apply_clearance(cart)
   # code here
